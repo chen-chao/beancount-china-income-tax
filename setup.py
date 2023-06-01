b22a59309da47f1a1196d57c8bed969ec5a2c2ca
Chao Chen <wenbushi@gmail.com>
2023-06-01 14:11:53 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,30 @@
-
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-import codecs
-
-with codecs.open('readme.md', encoding="utf-8") as fp:
+with open('readme.md', 'r', encoding="utf-8") as fp:
     long_description = fp.read()
-INSTALL_REQUIRES = [
-    'beancount>=2.3.5',
-]
 
 setup_kwargs = {
     'name': 'beancount-china-income-tax',
-    'version': '0.1.0',
+    'version': '0.1.1',
     'description': 'A beancount plugin to calculate and validate china personal income tax',
     'long_description': long_description,
     'license': 'GPL-2.0',
-    'author': '',
+    'author': 'Chao Chen',
     'author_email': 'Chao Chen <wenbushi@gmail.com>',
     'maintainer': None,
     'maintainer_email': None,
-    'url': '',
+    'url': 'https://github.com/chen-chao/beancount-china-income-tax',
     'packages': [
         'beancount-china-income-tax',
     ],
     'package_data': {'': ['*']},
     'long_description_content_type': 'text/markdown',
-    'install_requires': INSTALL_REQUIRES,
+    'install_requires': [
+        'beancount>=2.3.5',
+    ],
     'python_requires': '>=3.7',
 
 }
-
 
 setup(**setup_kwargs)

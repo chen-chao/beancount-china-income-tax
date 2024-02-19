1ad11a2ce07a5c4de99c3f47a231435c877460cf
Chao Chen <wenbushi@gmail.com>
2024-02-19 16:08:06 +0800

    add diagnostic information

--- src/beancount-china-income-tax/china_income_tax.py
+++ src/beancount-china-income-tax/china_income_tax.py
@@ -8,12 +8,12 @@ from beancount.core import data
 from beancount.core.number import ZERO, D
 from beancount.core import getters
 
-__plugins__ = ('china_income_tax',)
+__plugins__ = ("china_income_tax",)
 
-ChinaIncomeTaxError = namedtuple('ChinaIncomeTaxError', 'source message entry')
+ChinaIncomeTaxError = namedtuple("ChinaIncomeTaxError", "source message entry")
 
 # tax form
-TAX_ROW = namedtuple('TAX_ROW', ['level', 'rate', 'deduction'])
+TAX_ROW = namedtuple("TAX_ROW", ["level", "rate", "deduction"])
 TAX_TABLE = [
     TAX_ROW(D(36000), D(0.03), ZERO),
     TAX_ROW(D(144000), D(0.10), D(2520)),
@@ -26,8 +26,9 @@ TAX_TABLE = [
 
 # constants
 DEFAULT_MONTHLY_DEDUCTION = D(-5000)
-DEFAULT_PRECISION = '0.01'
-TAX_DEDUCTION = 'tax-deduction'
+DEFAULT_PRECISION = "0.01"
+TAX_DEDUCTION = "tax-deduction"
+
 
 def china_income_tax(entries, options_map, config):
     """calculate income tax from beancount transactions"""
@@ -38,14 +39,16 @@ def china_income_tax(entries, options_map, config):
 
     # configs
     config = get_config(config)
-    category = config['category']
-    tax_account = config['account']
-    monthly_deduction = config.get('monthly-deduction', DEFAULT_MONTHLY_DEDUCTION)
-    precision = D(config.get('precision', DEFAULT_PRECISION))
+    category = config["category"]
+    tax_account = config["account"]
+    monthly_deduction = config.get(
+        "monthly-deduction", DEFAULT_MONTHLY_DEDUCTION
+    )
+    precision = D(config.get("precision", DEFAULT_PRECISION))
 
     # process beancount threads
     for e in entries:
-        if e.meta.get('category', None) == category:
+        if e.meta.get("category", None) == category:
             if isinstance(e, data.Open):
                 taxable_accounts.add(e.account)
 
@@ -64,9 +67,11 @@ def china_income_tax(entries, options_map, config):
         deduction = monthly_deduction + D(t.meta.get(TAX_DEDUCTION, 0))
         income = ZERO
         tax = ZERO
+        taxed_accounts = []
         for p in t.postings:
             if p.account in taxable_accounts:
                 income += p.units.number
+                taxed_accounts.append(p.account)
 
             if p.account.startswith(tax_account):
                 tax += p.units.number
@@ -76,12 +81,23 @@ def china_income_tax(entries, options_map, config):
         acc_income[year] = acc_current
         acc_tax[year] += tax
 
-        if (tax_calculated.quantize(precision) != tax.quantize(precision)):
+        if tax_calculated.quantize(precision) != tax.quantize(precision):
             errors.append(
                 ChinaIncomeTaxError(
                     t.meta,
-                    f'income tax does not match, calculated: {tax_calculated:.2f}, actual: {tax:.2f}',
-                    t))
+                    (
+                        f"Income tax does not match. "
+                        f"Calculated: {tax_calculated:.2f}, "
+                        f"Actual: {tax:.2f}; \n\n"
+                        f"Diagnostics:\n"
+                        f"Accumulated income: {acc_current:.2f}, "
+                        f"Accumulated tax: {acc_tax[year]:.2f}, "
+                        f"Current income: {income:.2f} ({', '.join(taxed_accounts)}), "
+                        f"Deduction: {deduction:.2f}"
+                    ),
+                    t,
+                )
+            )
 
     return entries, errors
 
@@ -90,8 +106,8 @@ def get_config(config):
     """get key value pair from beancount plugin options"""
     d = {}
     if config:
-        for opt in config.split(','):
-            k, v = opt.split('=')
+        for opt in config.split(","):
+            k, v = opt.split("=")
             d[k] = v
 
     return d
@@ -107,4 +123,5 @@ def calc_tax(amount):
             return amount * t.rate - t.deduction
 
     raise ValueError(
-        f'calc_tax: invalid tax rate for {amount}. should not reach here')
+        f"calc_tax: invalid tax rate for {amount}. should not reach here"
+    )
